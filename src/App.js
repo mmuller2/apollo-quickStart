ca7b027e1c2c61c18eb8f4c879a9fb85f1407e46
MaxMuller Personal <fasttegr@gmail.com>
2022-06-22 11:46:37 +0100

    notes

--- src/App.js
+++ src/App.js
@@ -6,23 +6,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-        <p>
-          This is a test 
-        </p>
+      <a
+          className="App-link"
+          href="https://www.apollographql.com/docs/react/get-started/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          This was created from
+        </a>
         <div>
           <h2>My first Apollo app!!!</h2>
           <hr></hr>
           <h3>Exchange rates</h3>
           <ExchangeRates/>
         </div>
-        <a
-          className="App-link"
-          href="https://www.apollographql.com/docs/react/get-started/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn apollo
-        </a>
+        
       </header>
     </div>
   );
