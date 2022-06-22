3cb39d97593eb3c0c3c33cc4c6fa6a0dc7c61d66
MaxMuller Personal <fasttegr@gmail.com>
2022-06-22 11:33:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
+import ExchangeRates from './index'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a test 
         </p>
+        <div>
+          <h2>My first Apollo app!!!</h2>
+          <hr></hr>
+          <h3>Exchange rates</h3>
+          <ExchangeRates/>
+        </div>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.apollographql.com/docs/react/get-started/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn apollo
         </a>
       </header>
     </div>
